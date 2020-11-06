7767f08183e6d0d5f625c70408bf6ddc2925102e
Hoa-Vo <dev.h1620@gmail.com>
2020-11-06 18:04:24 +0700

    fix redirect and history

--- newterminal.c
+++ newterminal.c
@@ -154,9 +154,10 @@ void execCommand(char* command)
 {
 
     char* argsList[MAXLEN/2 +1]; 
-    int numberOfArg = parseToArgumentsList(argsList,command); 
-    
-    for (int i = 0;i<numberOfArg;i++){
+    int numberOfArg = parseToArgumentsList(argsList,command);
+    int isRedirectInput = 0;
+    for (int i = 0; i < numberOfArg; i++)
+    {
         if(strcmp(argsList[i],">")==0){
             historyCommand[historyIndex] = (char*)malloc(sizeof(char) * MAXLEN); 
             strcpy(historyCommand[historyIndex++], command);
@@ -166,11 +167,10 @@ void execCommand(char* command)
         else if(strcmp(argsList[i],"<")==0){
             historyCommand[historyIndex] = (char*)malloc(sizeof(char) * MAXLEN); 
             strcpy(historyCommand[historyIndex++], command);
+            isRedirectInput = 1;
             redirectInput(argsList, i);
             return;
         }
-        
-          
     }
     if(strcmp(command,"history") == 0){
         if(historyIndex>0&&strcmp(historyCommand[historyIndex-1],"history")){
@@ -197,7 +197,7 @@ void execCommand(char* command)
 
     execCommandWithArgumetsList(argsList, numberOfArg);
 
-    if(strcmp(command, "!!") != 0)
+    if(strcmp(command, "!!") != 0&&isRedirectInput)
     {
         historyCommand[historyIndex] = (char*)malloc(sizeof(char) * MAXLEN); 
         strcpy(historyCommand[historyIndex++], command); 
@@ -227,6 +227,7 @@ void redirectOutput(char** argsList,int pos){
     dup2(file_desc, 1) ;           
     execCommand(command);
     fclose(fout);
+    close(file_desc);
     //Back to output terminal
     dup2(saved_stdout, 1);
     close(saved_stdout);
@@ -238,10 +239,23 @@ void redirectInput(char **argsList, int pos){
     for (int i = 0; i < pos; i++)
     {
         command = strcatOverride(command,argsList[i]);
-        command = strcatOverride(command," ");
+        if(pos>1&&i<pos-1){
+           command = strcatOverride(command," ");
+        }
     }
-    command = strcatOverride(command, filename);
-    execCommand(command);
+    int saved_stdout;
+    saved_stdout = dup(0);
+    int fin = open(filename, O_RDONLY);
+    if(fin < 0){
+        printf("bash: %s: No such file or directory\n", filename);
+    }
+    else{
+        dup2(fin, STDIN_FILENO);
+        execCommand(command);
+    }
+    close(fin);
+    dup2(saved_stdout, 0);
+    close(saved_stdout);
 }
 
 char *strcatOverride(char *a, char *b) {
